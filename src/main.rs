8c955597fabb9babccd29e86973f4f91b84cf2e8
laurent bernabé <laurent.bernabe@gmail.com>
2025-07-24 16:53:59 +0200

    show ferris image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::Svg;
+
+fn main() -> iced::Result {
+    iced::run("Svg experiment", MyApp::update, MyApp::view)
+}
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+#[derive(Default)]
+struct MyApp {}
+
+impl MyApp {
+    fn update(&mut self, _message: Message) {}
+
+    fn view(&self) -> iced::Element<Message> {
+        Svg::from_path(format!("{}/assets/ferris.svg", env!("CARGO_MANIFEST_DIR")))
+            .width(100)
+            .height(100)
+            .into()
+    }
 }
