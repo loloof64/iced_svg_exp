b98fda96b65c4a794479dc9e7b94969ebadae244
laurent bernabé <laurent.bernabe@gmail.com>
2025-07-24 16:56:59 +0200

    Ferris is bigger

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use iced::widget::Svg;
+use iced::{Length, widget::Svg};
 
 fn main() -> iced::Result {
     iced::run("Svg experiment", MyApp::update, MyApp::view)
@@ -7,16 +7,23 @@ fn main() -> iced::Result {
 #[derive(Debug, Clone)]
 enum Message {}
 
-#[derive(Default)]
-struct MyApp {}
+struct MyApp {
+    ferris_size: f32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self { ferris_size: 300.0 }
+    }
+}
 
 impl MyApp {
     fn update(&mut self, _message: Message) {}
 
     fn view(&self) -> iced::Element<Message> {
         Svg::from_path(format!("{}/assets/ferris.svg", env!("CARGO_MANIFEST_DIR")))
-            .width(100)
-            .height(100)
+            .width(Length::Fixed(self.ferris_size))
+            .height(Length::Fixed(self.ferris_size))
             .into()
     }
 }
