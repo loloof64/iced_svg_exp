9025f717bf382401fdc18dfe6dce21541869ef08
laurent bernabé <laurent.bernabe@gmail.com>
2025-07-24 17:07:45 +0200

    we can resize ferris

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use iced::{Length, widget::Svg};
+use iced::{
+    Length,
+    widget::{Svg, column, slider},
+};
 
 fn main() -> iced::Result {
     iced::run("Svg experiment", MyApp::update, MyApp::view)
 }
 
 #[derive(Debug, Clone)]
-enum Message {}
+enum Message {
+    UpdateSize(f32),
+}
 
 struct MyApp {
     ferris_size: f32,
@@ -18,12 +23,19 @@ impl Default for MyApp {
 }
 
 impl MyApp {
-    fn update(&mut self, _message: Message) {}
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::UpdateSize(new_size) => self.ferris_size = new_size,
+        }
+    }
 
     fn view(&self) -> iced::Element<Message> {
-        Svg::from_path(format!("{}/assets/ferris.svg", env!("CARGO_MANIFEST_DIR")))
-            .width(Length::Fixed(self.ferris_size))
-            .height(Length::Fixed(self.ferris_size))
-            .into()
+        column![
+            slider(100.0..=600.0, self.ferris_size, Message::UpdateSize),
+            Svg::from_path(format!("{}/assets/ferris.svg", env!("CARGO_MANIFEST_DIR")))
+                .width(Length::Fixed(self.ferris_size))
+                .height(Length::Fixed(self.ferris_size))
+        ]
+        .into()
     }
 }
